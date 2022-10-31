5c2e10a3e00c19730504f2431bf76e92c5257077
wearrqqws <rowancole19@gmail.com>
2022-10-31 14:33:57 -0500

    Change Font

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const { appWindow } = window.__TAURI__.window
+
+await appWindow.center();
+// Window controls 
+document.querySelector('.winBtnClose').addEventListener('click', () => {
+  appWindow.close()
+})
+document.querySelector('.winBtnMax').addEventListener("click", () => {
+  appWindow.toggleMaximize()
+})
+document.querySelector('.winBtnMin').addEventListener("click", () => {
+  appWindow.minimize()
+})
+
+
+let userID = "Bob"
+function updateUserID() {
+    document.getElementById("userID").textContent = userID
+}
+updateUserID();
