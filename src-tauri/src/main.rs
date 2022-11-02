356fb447838108b7202959473073b6da84d8f6b6
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-11-02 17:31:14 -0400

    Added Taskbar Menu to Tauri App

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,56 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn hello_rust() {
-  println!("I was invoked from JS!");
-}
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent, SystemTrayMenuItem};
+use tauri::Manager;
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![hello_rust])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    
+  let connect = CustomMenuItem::new("connect".to_string(), "Connect");
+  let settings = CustomMenuItem::new("settings".to_string(), "Settings");
+  let show = CustomMenuItem::new("show".to_string(), "Hide");
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+
+  let tray_menu = SystemTrayMenu::new()
+  .add_item(connect)
+  .add_item(settings)
+  .add_native_item(SystemTrayMenuItem::Separator)
+  .add_item(show)
+  .add_item(quit);
+
+  tauri::Builder::default()
+    .system_tray(SystemTray::new().with_menu(tray_menu))
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::MenuItemClick { id, .. } => {
+        let item_handle = app.tray_handle().get_item(&id);
+        match id.as_str() {
+          "connect" => {
+            // connect vpn/disconnect
+
+            item_handle.set_title("Disconnect").unwrap();
+          }
+
+          "settings" => {
+            // open settings window or something
+          }
+
+          "show" => {
+            //toggle show/hide window
+
+            item_handle.set_title("Show").unwrap();
+          }
+
+          "quit" => {
+            // ...quit
+          }
+          _ => {}
+        }
+      }
+      _ => {}
+    })
+    .build(tauri::generate_context!())
+    .expect("error while running tauri application")
+    .run(|_app_handle, _event| match _event {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+          api.prevent_exit();
+        }
+        _ => {}
+    });
 }
